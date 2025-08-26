22c2137051f64ac460890969ae18cd6cee96c4e8
Sleleu <93100775+Sleleu@users.noreply.github.com>
2025-08-26 18:11:18 +0200

    add playbook/start_inception.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,19 @@ aws configure set output json --profile "$AWS_USER"
 # Install amazon.aws ansible plugin
 ansible-galaxy collection install amazon.aws
 
+# Wait to get instances
+ansible all -m wait_for_connection -a "timeout=300"
+
+echo "Launching playbooks"
+
+# Launch playbooks
+echo "playbook/export_inception.yml"
+ansible-playbook playbook/export_inception.yml -v
+echo "playbook/install_dependencies.yml"
+ansible-playbook playbook/install_dependencies.yml -v
+echo "playbook/start_inception.yml"
+ansible-playbook playbook/start_inception.yml -v
+echo "playbooks successfully executed !"
+
 # Keep container alive
 tail -f /dev/null
